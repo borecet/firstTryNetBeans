9ec788b766bfa92b59dd877155952030f73f4d50
lecso <lecso@ap201801-Latitude-E5430-vPro>
2018-05-04 11:23:39 +0200

    alap_tabla_a1

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,15 +1,35 @@
-
-for (i=1;i<9;i++){
+var tableRow, tableColumn, fieldName, field;
+for (i=8;i>0;i--){
 var x = document.createElement("TR");
     x.setAttribute("id", "Tr"+i);
     document.getElementById("chessTable").appendChild(x);
     for (j=1;j<9;j++){
-    var y = document.createElement("TD");
-    var k = 10*j+9-i;
-        var t = document.createTextNode(k);
-    
-    y.setAttribute("id","td"+k);
-    y.appendChild(t);
-    document.getElementById("Tr"+i).appendChild(y);
+    field = document.createElement("TD");
+    switch(j){
+        case 1 : tableColumn="a";
+            break;
+        case 2 : tableColumn="b";
+            break;
+        case 3 : tableColumn="c";
+            break;
+        case 4 : tableColumn="d";
+            break;    
+        case 5 : tableColumn="e";
+            break;
+        case 6 : tableColumn="f";
+            break;
+        case 7 : tableColumn="g";
+            break;
+        default : tableColumn="h";
+           
+    }
+    var fieldName = tableColumn+i;
+        var t = document.createTextNode(fieldName);
+    if((i+j)/2===Math.floor((i+j)/2)){
+        field.style.backgroundColor="black";
+    };
+    field.setAttribute("id",fieldName);
+    field.appendChild(t);
+    document.getElementById("Tr"+i).appendChild(field);
          }
     }

f8134b1c897259a5240244c078e2f37cc1e04a94
lecso <lecso@ap201801-Latitude-E5430-vPro>
2018-05-04 10:57:44 +0200

    alap_tabla_11

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,15 @@
 
+for (i=1;i<9;i++){
+var x = document.createElement("TR");
+    x.setAttribute("id", "Tr"+i);
+    document.getElementById("chessTable").appendChild(x);
+    for (j=1;j<9;j++){
+    var y = document.createElement("TD");
+    var k = 10*j+9-i;
+        var t = document.createTextNode(k);
+    
+    y.setAttribute("id","td"+k);
+    y.appendChild(t);
+    document.getElementById("Tr"+i).appendChild(y);
+         }
+    }
